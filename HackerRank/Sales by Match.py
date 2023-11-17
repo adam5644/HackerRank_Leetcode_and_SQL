aef9a68cb95f2b53fa11a951cc9bbd244433616b
adam5644 <97981820+adam5644@users.noreply.github.com>
2023-11-17 23:09:17 +0800

    Add files via upload

---
+++ HackerRank/Sales by Match.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    set1 = set(ar)
+    res = 0
+    for each in set1:
+        res += ar.count(each) // 2
+        
+    return res
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
