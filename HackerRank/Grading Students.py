b8077eb1cb65fdbf1a43d84bc40dedfb2ea3f0e5
adam5644 <97981820+adam5644@users.noreply.github.com>
2023-11-17 19:16:32 +0800

    Add files via upload

---
+++ HackerRank/Grading Students.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    res = []
+    
+    for each in grades:
+        if each < 38:
+            res.append(each)
+        else:
+            upper_5 = 5*(int(each//5)+1)
+            if upper_5 - each <= 2:
+                each = upper_5
+            res.append(each)
+    return res
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
