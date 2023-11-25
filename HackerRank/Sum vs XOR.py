ca981994bd01b93e904d5486c29b5f877e16fe53
(Adam) Chen Longhui <chen1257@e.ntu.edu.sg>
2023-11-25 14:06:15 +0800

    Add files via upload

---
+++ HackerRank/Sum vs XOR.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def sumXor(n):
+    c = 0 
+    for x in range(n+1): # o(n)
+        a = n + x
+        b = n^ x
+        if a == b:
+            c += 1
+
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
