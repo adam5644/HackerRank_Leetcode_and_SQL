94d83c3ef5deaacd7609b2c543c3156814377148
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-13 07:55:24 +0800

    Add files via upload

--- HackerRank/Ice Cream Parlor.py
+++ HackerRank/Ice Cream Parlor.py
@@ -15,61 +15,48 @@ import sys
 #  2. INTEGER_ARRAY arr
 #
 
-def icecreamParlor(m, arr):
-    # print()
-    # print('start')
-    # print('m = ', m)
-    # print('arr = ', arr)
-    store = {}
-    for i in range(len(arr)):
-        left = m - arr[i]
-        # print('left = ', left)
-        if left in store:
-            res = [store[left]+1, i+1]
-            # print('res = ', res)
-            return res
-        store[arr[i]] = i
-        # print(store)
+def icecreamParlor(m, arr): # m=6, cost = [1,3,4,5,6]
+    arr = [int(i) for i in arr]
+    arr_dict = {flavor+1: cost  for flavor, cost in enumerate(arr)}
+    arr_dict2 = {cost: flavor+1  for flavor, cost in enumerate(arr)}
+        
+    for flavor, cost in arr_dict.items():
+        left = m - cost
+        flavor1 = flavor
+        
+        if left in arr_dict2.keys():
+            flavor2 = arr_dict2[left]
+            
+            if flavor1 < flavor2:
+                return f'{flavor1} {flavor2}'
+            else:
+                return f'{flavor2} {flavor1}'
+        
+    
+    
+    # res sort ascending
 
 if __name__ == '__main__':
-    f = open(os.environ['OUTPUT_PATH'], 'w')
-    t = int(input().strip())
+    f=open(os.environ['OUTPUT_PATH'],'w')
     
-    for _ in range(t):    
-        m = int(input().strip())
-        n = int(input().strip())
-        cost = []
-        # cost = (int(input().strip().split()))
-        cost = list(map(int,input().split()))
-        res = icecreamParlor(m, cost) 
-        res2 = str(res[0])
-        for i in range(1,len(res)):
-            res2+=f' {res[i]}'
-        f.write(res2 + '\n')
+    t = int(input())
+    for _ in range(t):
+        m = int(input())
+        n = int(input())
+        arr = input().split()
+        # print('m = ', m)
+        # print('n = ', n)
+        # print('arr = ', arr)
+        # m =  4
+        # n =  5
+        # arr =  ['1', '4', '5', '3', '2']
         
+        res = icecreamParlor(m, arr)
+        f.write(res + '\n')
+    
+    
     f.close()
     
-    #####################
-    
-    # # res = '1 4'
-    # res = [1,4]
-    # res2 = str(res[0])
-    # for i in range(1,len(res)):
-    #     res2+=f' {res[i]}'
-        
-    # f.write(res2 + '\n')
-    
-    # #res = '1 2'
-    # res = [1,2]
-    # res2 = str(res[0])
-    # for i in range(1,len(res)):
-    #     res2+=f' {res[i]}'
-    
-    # f.write(res2 + '\n')
-    
-    # f.close()
-            
-
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     # t = int(input().strip())
