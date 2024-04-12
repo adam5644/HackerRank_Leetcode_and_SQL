5b4a4fe17c22a30285355cc27923547b9f97eff5
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-12 22:46:59 +0800

    Add files via upload

--- HackerRank/Two Characters.py
+++ HackerRank/Two Characters.py
@@ -14,27 +14,62 @@ import sys
 #
 
 def alternate(s):
-    unique_characters = set(s)
-    max_length = 0
+    from itertools import combinations
+    combi = combinations(set(s),2)
+    
+    res = 0
+    for c in combi:
+        s2 = [_ for _ in s if _ in c]
+        if all(s2[i] != s2[i+1] for i in range(len(s2)-1)):
+            res = max(res, len(s2))
+    return res
+        
+        
+        
+        
+    #     s2 = s
+    #     s2 = s2.replace(c[0],'')
+    #     s2 = s2.replace(c[1],'')
+    #     if len(s2) <= 1: # assume 'aaaa' is not valid
+    #         continue
+    #     if len(set(s2)) >= 3:
+    #         continue
+    #     if s2[0] == s2[1]:
+    #         continue
+    #     a = s2[0]
+    #     b = s2[1]
+    #     c = a
+    #     for i in range(2, len(s2)):
+    #         if s2[i] != c:
+    #             break
+    #         if c==a: c =b
+    #         else: c = a
+    #         res = max(res, len(s2))
+    # return res
+            
 
-    for char1 in unique_characters:
-        for char2 in unique_characters:
-            if char1 != char2:
-                filtered = [c for c in s if c == char1 or c == char2]
-                if all(filtered[i] != filtered[i+1] for i in range(len(filtered) - 1)):
-                    max_length = max(max_length, len(filtered))
 
-    return max_length
 
+    
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    l = int(input().strip())
-
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    length_of_s = int(input())
+    
     s = input()
-
+    
     result = alternate(s)
+    f.write(str(result))
+    f.close()
 
-    fptr.write(str(result) + '\n')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    fptr.close()
+    # l = int(input().strip())
+
+    # s = input()
+
+    # result = alternate(s)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
