a20f866aec7e105c56fa5d6b10958507b432d3a3
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-13 16:12:14 +0800

    Add files via upload

---
+++ HackerRank/Larry's Array.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'larrysArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY A as parameter.
+#
+
+def larrysArray(A):
+    res = 0 
+    inversion = 0
+    
+    for i in range(len(A)):
+        for j in range(i):
+            if A[j] > A[i]:
+                res += 1
+    if res %2 == 0:
+        return 'YES'
+    else:
+        return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        A = list(map(int, input().rstrip().split()))
+
+        result = larrysArray(A)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
