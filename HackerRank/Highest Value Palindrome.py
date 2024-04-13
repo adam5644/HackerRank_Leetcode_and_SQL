7637c5fbe3f36959b96c856d196c2a15befea9e6
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-13 23:21:21 +0800

    Add files via upload

--- HackerRank/Highest Value Palindrome.py
+++ HackerRank/Highest Value Palindrome.py
@@ -1,79 +1,36 @@
 def highestValuePalindrome(s, n, k):
-
-
-    leng = len(s)
+    s = list(s)
+    # Track changes needed to make palindrome
+    diff = [0] * (n // 2)
     
-    # base
-    if leng %2 ==0: 
-        a, b = s[:leng//2], s[leng//2:]
-    else:
-        a,b = s[:leng//2], s[leng//2+1:]
+    for i in range(n // 2):
+        if s[i] != s[-1 - i]:
+            diff[i] = 1
     
-    # a_idx, b_idx
-    diff = 0
-    leng2 = len(a)
-    i, j=0, (leng2-1)
+    # Make palindrome
+    for i in range(n // 2):
+        if diff[i] == 1:
+            if k > 0:
+                higher = max(s[i], s[-1 - i])
+                s[i], s[-1 - i] = higher, higher
+                k -= 1
+            else: 
+                return '-1'
     
-    a_idx, b_idx = [], []
-    for _ in range(leng2):
-        if a[i] != b[j]:
-            a_idx.append(i)
-            b_idx.append(j)
-            diff += 1
-        i += 1
-        j -= 1
+    # Optimize to make the value the highest possible
+    for i in range(n // 2):
+        if k <= 0:
+            break
+        if s[i] != '9':
+            if diff[i] == 1 and k >= 1:
+                s[i] = s[-1 - i] = '9'
+                k -= 1
+            elif diff[i] == 0 and k >= 2:
+                s[i] = s[-1 - i] = '9'
+                k -= 2
     
-    # print('a = ',a)
-    # print('b = ', b)
-    # print('a_idx = ', a_idx)
-    # print('b_idx = ', b_idx)
+    # Handle odd length case
+    if n % 2 == 1 and k >= 1:
+        s[n // 2] = '9'
     
-    # edge
-    if diff > k:
-        return '-1'
-    
-    # form a palindrome
-    res = ''
-    #print('start res = ', res)
-    extra = k - diff
-    #print('extra = ',extra)
-    i, j=0, (leng2-1)
-    for _ in range(leng2):
-        if i not in a_idx:
-            res += a[i]
-        else:
-            if a[i] > b[j]:
-                if extra > 0:
-                    res+='9'
-                    extra -= 1
-                else:
-                    res+=a[i]
-            else:
-                if extra > 0:
-                    res+='9'
-                    extra -= 1
-                else:
-                    res+=b[i]             
-        i += 1
-        j -= 1
-    
-    #print('end res = ', res)
-    
-    # extra
-    while extra >= 2:
-        for x in res:
-            if x != '9':
-                res = res.replace(x,'9',1)
-                break
-        extra-=2
-    
-    # return
-    if leng %2 ==1:
-        if extra >= 1:
-            #print('2 res = ', res)
-            return res + '9' + res[::-1]
-        else:
-            return res + s[leng//2] + res[::-1]
-            
-    #print('3 res = ', res)
-    return res+res[::-1]
+    return ''.join(s)
