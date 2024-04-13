e709707fc72461c3ff996ff0b3d20f2805062558
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-14 07:30:41 +0800

    Add files via upload

--- HackerRank/Sherlock and Anagrams.py
+++ HackerRank/Sherlock and Anagrams.py
@@ -5,7 +5,6 @@ import os
 import random
 import re
 import sys
-from collections import defaultdict
 
 #
 # Complete the 'sherlockAndAnagrams' function below.
@@ -14,48 +13,27 @@ from collections import defaultdict
 # The function accepts STRING s as parameter.
 #
 
+from collections import defaultdict
+import math
+
 def sherlockAndAnagrams(s):
-    # a = defaultdict(int)
-    # s = 'abba'
-    # print(a[s])
-
-    # a = 'abba'
-    # b = sorted(a)
-    # c = ''
-    # for x in b:
-    #     c+=x
-    # print('c = ', c)
-    # print(type(c))
-
-    # print()
-    # print()
-    # print('start')
-    res = 0
-    for n in range(1, len(s)):
-        store = defaultdict(int) # sort before storing
-        
-        for i in range(len(s)):
-            if i+n-1 >= len(s):
-                break #breaks 'for i' loop
-            
-            curr = s[i:i+n]
-            
-            b = sorted(curr)
-            c = ''
-            for x in b:
-                c+=x
-            
-            # print('c = ', c)
-            # print('store = ', store)
-            
-            res += store[c]
-            # print('res = ', res)
-            
-            store[c] += 1
-            
-    return res
+    res = 0 
+    # all possible substring
+    d = defaultdict(int)
     
-    
+    n = len(s)
+    for i in range(n):
+        for j in range(i+1, n+1):
+            substr = ''.join(sorted(s[i:j]))
+            #print('substr = ', substr)
+            d[substr]+=1
+            
+    for count in d.values():
+        if count >=2:
+            res += count * (count - 1) // 2
+            
+    return int(res)
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
