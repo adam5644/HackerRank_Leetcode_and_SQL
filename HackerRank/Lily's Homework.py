4d0938556778ba25049627ecd7e6698b25aaf52c
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-13 21:31:51 +0800

    Add files via upload

--- HackerRank/Lily's Homework.py
+++ HackerRank/Lily's Homework.py
@@ -1,19 +1,54 @@
-def countSwaps(arr, r):
-    arr = arr.copy()
-    target = sorted(arr, reverse = r)
-    inds = {v:i for i,v in enumerate(target)}
-    i = 0
-    count = 0
-    while i < len(arr) and arr != target:
-        while arr[i] != target[i]:
-            ind = inds[arr[i]]
-            arr[i], arr[ind] = arr[ind], arr[i]
-            count += 1
-            print('count = ', count)
-            print('arr = ', arr)
-        i += 1
-    return count
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lilysHomework' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def calc_swaps_num(arr, target):
+    # To avoid modifying the original array passed to calc_swaps_num, work on a copy
+    arr = arr[:]
+    # Map each element to its index in the target sorted array for quick lookup
+    target_index = {value: idx for idx, value in enumerate(target)}
+    swaps = 0
+    for i in range(len(arr)):
+        # Correct position of current element in the target array
+        correct_pos = target_index[arr[i]]
+        # If element is not in the correct position, swap it with the element that is currently in its correct position
+        while i != correct_pos:
+            # Position of the element that should be at i
+            to_swap_idx = target_index[arr[i]]
+            # Swap elements
+            arr[i], arr[to_swap_idx] = arr[to_swap_idx], arr[i]
+            # Update the position, since arr[i] has changed
+            correct_pos = target_index[arr[i]]
+            swaps += 1
+    return swaps
 
 def lilysHomework(arr):
-    return min(countSwaps(arr, False), countSwaps(arr, True))
- 
+    target1 = sorted(arr)
+    target2 = sorted(arr, reverse=True)
+    
+    return min(calc_swaps_num(arr, target1),
+               calc_swaps_num(arr, target2))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = lilysHomework(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
