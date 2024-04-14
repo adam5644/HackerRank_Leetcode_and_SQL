c6726e2d5545fa120810a95dec2c79681ce16395
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-14 15:23:01 +0800

    Add files via upload

--- HackerRank/The Maximum Subarray.py
+++ HackerRank/The Maximum Subarray.py
@@ -1,56 +1,15 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'maxSubarray' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
 def maxSubarray(arr):
+    # Maximum subarray sum using Kadane's Algorithm
+    max_ending_here = max_so_far = arr[0]
+    for num in arr[1:]:
+        max_ending_here = max(num, max_ending_here + num)
+        max_so_far = max(max_so_far, max_ending_here)
     
-    if max(arr) < 0:
-        return [max(arr),max(arr)]
+    # Maximum subsequence sum
+    # If all elements are negative, return the maximum element; otherwise, sum all positive elements.
+    if all(x < 0 for x in arr):
+        max_subseq = max(arr)
+    else:
+        max_subseq = sum(x for x in arr if x > 0)
     
-    max_subarray = sum([x for x in arr if x>0])
-    
-    n = len(arr)
-    
-    dp = [0]*n # dp[i] is the cumulative max for optimal subsequence so far that ends at index i. 
-    
-    for i in range(len(dp)):
-        dp[i] = max(0, dp[i-1] + arr[i])
-        
-    max_subsequence = max(dp)
-        
-    '''
-    arr = [-1,2,3,-4,5,10]
-    res = [0,2,5,1,6,16]
-    '''
-        
-    
-    return [max_subsequence, max_subarray]
-    # return [-1, -1]
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input().strip())
-
-    for t_itr in range(t):
-        n = int(input().strip())
-
-        arr = list(map(int, input().rstrip().split()))
-
-        result = maxSubarray(arr)
-
-        fptr.write(' '.join(map(str, result)))
-        fptr.write('\n')
-
-    fptr.close()
+    return [max_so_far, max_subseq]
