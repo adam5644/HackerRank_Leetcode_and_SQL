57f96349454fe24b1414d02a233c9f96447ba21c
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-15 10:23:10 +0800

    Add files via upload

--- HackerRank/The Coin Change Problem.py
+++ HackerRank/The Coin Change Problem.py
@@ -1,9 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
+
 def getWays(n, c):
-    res = [0] * (n + 1)  
+    # dp[i] will be storing the number of solutions for
+    # value i. We need n+1 rows as the table is constructed
+    # in bottom up manner using the base case (n = 0)
+    dp = [0] * (n + 1)
 
-    res[0] = 1  # There is one way to make 0 with 0 coins
-    for coin in c:  # Iterate over each coin
-        for amount in range(coin, n + 1):  # Update the res array for all amounts from coin to n
-            res[amount] += res[amount - coin]
+    # Base case (If given value is 0)
+    dp[0] = 1
 
-    return res[n]
+    # Pick all coins one by one and update the dp[] values
+    # after the index greater than or equal to the value of the
+    # picked coin
+    for coin in c:
+        for i in range(coin, n + 1):
+            dp[i] += dp[i - coin]
+
+    return dp[n]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, c)
+
+    fptr.write(str(ways) + '\n')
+
+    fptr.close()
