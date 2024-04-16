e22aa50142fec0ee26ae6b398c31d660560abd72
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-16 08:15:30 +0800

    Add files via upload

--- HackerRank/Stock Maximize.py
+++ HackerRank/Stock Maximize.py
@@ -1,13 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
 def stockmax(prices):
+    # Write your code here
+    peak = prices[-1]
     res = 0
-    max_so_far = 0
-
-    for i in range(len(prices) - 1, -1, -1):  # Traverse from right to left
-        if prices[i] > max_so_far:
-            max_so_far = prices[i]  # Update max_so_far if current price is greater
+    for i in range(len(prices)-2,-1,-1):
+        curr=prices[i]
+        #print('curr = ', curr)
+        change=peak-curr
+        #print('change = ', change)
+        if change>0:
+            res+=change
         else:
-            res += max_so_far - prices[i]  # Calculate profit
-
+            peak=curr
     return res
 
-# Rest of the code remains unchanged
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
