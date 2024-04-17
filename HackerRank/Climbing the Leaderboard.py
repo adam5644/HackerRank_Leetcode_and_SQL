d3ae766f201f9f996f733d1eb6d02be466cdf28d
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-04-17 16:38:50 +0800

    Add files via upload

--- HackerRank/Climbing the Leaderboard.py
+++ HackerRank/Climbing the Leaderboard.py
@@ -15,66 +15,41 @@ import sys
 #  2. INTEGER_ARRAY player
 #
 
-# def climbingLeaderboard(ranked, player):
-#     '''
-#     for x in player:
-#         recalculate ranked
-#     * use set for unique iterable
-#     '''
-#     ranked = sorted(set(ranked),reverse=True) # o(n) for sorted
-#     res = []
-#     for x in player:
-#         if x in ranked:
-#             res.append(ranked.index(x)+1)
-#         else:
-#             ranked.append(x)
-#             print(ranked)
-#             ranked = sorted(ranked,reverse=True) 
-#             res.append(ranked.index(x)+1)
-#     return res
-
-def climbingLeaderboard(ranked, player):
-    # Remove duplicates and sort in descending order
-    ranked = sorted(set(ranked), reverse=True)
-    res = []
-    l = len(ranked)
-    start = l
-    
-    for x in player:
-        while start >= 0 and x>ranked[start-1]:
-            start-=1
-        res.append(start+1)
-    return res
-    # for x in player:
-    #     # Use a reverse loop to find the rank
-    #     while start > 0 and x >= ranked[start - 1]:
-    #         start -= 1
-    #     res.append(start + 1)
-
-    # return res
-            
-            
-
-    # for x in player:
-    # ranked.append(100)
-    # print(ranked)
-    
-    # ordered list?
+def climbingLeaderboard(scores, alice):
+    scores_set = list(set(scores))
+    scores_set.sort(reverse=True)
+    result = []
+    l = len(scores_set)
+    for s in alice:
+        while (l>0) and (s >= scores_set[l-1]):
+            l -= 1
+        result.append(l+1)
+    return result 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    f =open(os.environ['OUTPUT_PATH'], 'w')
+    _ = input()
+    ranked = list(map(int, input().strip().split()))
+    _ = input()
+    player = list(map(int, input().strip().split()))
+    res = climbingLeaderboard(ranked, player)
+    for i in res:
+        f.write(str(i) + '\n')
+    f.close()
+    
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    ranked_count = int(input().strip())
+    # ranked_count = int(input().strip())
 
-    ranked = list(map(int, input().rstrip().split()))
+    # ranked = list(map(int, input().rstrip().split()))
 
-    player_count = int(input().strip())
+    # player_count = int(input().strip())
 
-    player = list(map(int, input().rstrip().split()))
+    # player = list(map(int, input().rstrip().split()))
 
-    result = climbingLeaderboard(ranked, player)
+    # result = climbingLeaderboard(ranked, player)
 
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
+    # fptr.write('\n'.join(map(str, result)))
+    # fptr.write('\n')
 
-    fptr.close()
+    # fptr.close()
