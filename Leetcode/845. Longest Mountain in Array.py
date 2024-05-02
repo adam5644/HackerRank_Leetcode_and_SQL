80a9880eb191d3f374d66d7411c7a97368dae293
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-02 20:44:07 +0800

    Add files via upload

---
+++ Leetcode/845. Longest Mountain in Array.py
@@ -0,0 +1,18 @@
+ 
+
+
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        res = 0
+
+        for i in range(1, len(arr) - 1):
+            # if it is peak
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                l = r = i
+                while l > 0 and arr[l - 1] < arr[l]:
+                    l -= 1
+                while r < len(arr) - 1 and arr[r + 1] < arr[r]:
+                    r += 1
+                
+                res = max(res, (r - l + 1))
+        return res
