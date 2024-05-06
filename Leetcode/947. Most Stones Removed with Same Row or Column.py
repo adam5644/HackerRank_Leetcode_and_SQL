90ce7b39c4b2f36b5292127be2bff297f72b7d36
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-06 18:34:32 +0800

    Add files via upload

--- Leetcode/947. Most Stones Removed with Same Row or Column.py
+++ Leetcode/947. Most Stones Removed with Same Row or Column.py
@@ -1,17 +1,26 @@
-class Solution:
-  def removeStones(self, stones: List[List[int]]) -> int:
-    visited = set()
-    def dfs(i,j):     
-      if (i,j) in visited: 
-        return 0
-      visited.add((i,j))
-      ans=0
-      for x,y in stones:  
-        if (i==x or j==y) and (x,y) not in visited: 
-          ans+=1+dfs(x,y)       
-      return ans
+# 947. Most Stones Removed with Same Row or Column
 
-    ans=0
-    for x,y in stones:
-      ans+=(dfs(x,y))
-    return ans 
+class Solution:
+    def removeStones(self, stones) -> int:
+        
+        def dfs(x, y):
+            seen.add((x, y))
+            for x2, y2 in stones:
+                if (x == x2 or y == y2) and (x2, y2) not in seen:
+                    dfs(x2, y2)
+            
+        seen = set() # set of set
+        cnt = 0
+        
+        for x, y in stones:
+            if (x, y) not in seen:
+                dfs(x, y)
+                cnt += 1
+                
+        return len(stones) - cnt
+
+#stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+#stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+#stones = [[0,0]]
+stones = [[0,1],[1,1]] # 1
+print(Solution().removeStones(stones))
