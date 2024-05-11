39d1adc13e5b6bac25d17e7b1184d3b8be7864a7
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-11 13:06:20 +0800

    Add files via upload

--- HackerRank/Roads and Libraries.py
+++ HackerRank/Roads and Libraries.py
@@ -1,34 +1,54 @@
-def roadsAndLibraries(n, c_lib, c_road, cities):
-    # print(n)
-    # print(c_lib)
-    # print(c_road)
-    # print(cities)
-    
-    if c_lib<c_road:
-        return c_lib*n
-    city=[{i} for i in range(n+1)]
-    for [x,y] in cities:
-        if city[x]is not city[y]:
-            for k in city[y]:
-                city[x].add(k)
-                city[k]=city[x]
-                
-        print('x,y=',x,y)
-        print('city = ', city)
-        print()
-                
-        # if city[x]is not city[y]:
-        #     for k in city[y]:
-        #         city[x].append(k)
-        #         city[k]=city[x]
-                
-    cityset = set([id(c) for c in city[1:]])
-    print('cityset=',cityset)
-    print('len = ', len(cityset))
-    return len(cityset)*c_lib+(n-len(cityset))*c_road
+from collections import defaultdict
 
-n = 3
-c_lib = 2
-c_road = 1
-cities = [[1, 2], [3, 1], [2, 3]]
-print(roadsAndLibraries(n, c_lib, c_road, cities))
+q = int(input())
+for _ in range(q):
+    n,m,c_lib,c_road=map(int,input().rstrip().split())
+    
+    cities=[]
+    for _ in range(m):
+        cities.append(list(map(int,input().rstrip().split())))
+        
+    #print(n,c_lib,c_road, cities)
+    if c_lib<c_road: 
+        print(c_lib*n)
+        continue
+    if len(cities)==0:
+        print(c_lib*n)
+        continue
+    
+    # main
+    connections=defaultdict(list)
+    for i,j in cities:
+        connections[i-1].append(j-1)
+        connections[j-1].append(i-1)
+    #print('connections = ', connections)
+    
+    # main
+    vis=set()
+    clusters=[]
+    for i in range(n):
+        if i not in vis:
+            #print('i = ', i)
+            cluster=set([i]) # new cluster
+            breadth=set([i]) # breadht of i
+            while breadth:
+                j=breadth.pop() # j
+                if j not in vis:
+                    #print('j = ', j)
+                    cluster.add(j) # add j to cluster
+                    vis.add(j) # visited j
+                    breadth=breadth.union(set(connections[j])) # breadth of j
+            #print('cluster = ', cluster)
+            clusters.append(cluster) # add new cluster to clusters
+    #print('clusters = ', clusters)
+    
+    # main, clusters
+    res=0
+    res+=c_lib * len(clusters)
+    for x in clusters:
+        res+=((len(x)-1)*c_road)
+    print(res)
+    continue
+        
+    
+    
