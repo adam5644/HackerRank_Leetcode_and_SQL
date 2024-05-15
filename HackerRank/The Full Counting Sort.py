97a4ecdc321e0d58f612ad72e98f6241792cfb18
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-16 01:11:17 +0800

    Add files via upload

--- HackerRank/The Full Counting Sort.py
+++ HackerRank/The Full Counting Sort.py
@@ -1,55 +1,21 @@
-#!/bin/python3
+from collections import defaultdict
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'countSort' function below.
-#
-# The function accepts 2D_STRING_ARRAY arr as parameter.
-#
-
-def countSort(arr):
-
-# # [['0', 'ab'], ['6', 'cd'], ['0', 'ef'], ['6', 'gh'], ['4', 'ij'], ['0', 'ab'], ['6', 'cd'], ['0', 'ef'], ['6', 'gh'], ['0', 'ij'], ['4', 'that'], ['3', 'be'], ['0', 'to'], ['1', 'be'], ['5', 'question'], ['1', 'or'], ['2', 'not'], ['4', 'is'], ['2', 'to'], ['4', 'the']]
-    res = [[] for _ in range(200)]
-    n = len(arr) # 20 
-    half = n//2-1 # 0 to 9 turn to '-'
+n = int(input())
+d=defaultdict(list)
+for _ in range(n//2):
+    temp = input().strip().split()
+    i= int(temp[0])
+    d[i].append('-')
     
-    #print('half = ', half)
-    for index, string in enumerate(arr):
-        #print('index = ', index)
-        integer = int(string[0])
-        word = string[1]
-
-        if index <= half:
-            res[integer].append('-')
-            #print('-')
-        else:
-            res[integer].append(word)
-            #print('word = ',word)
-        #print()
-
-    res2 = ''
-       
+for _ in range(n//2):
+    temp = input().strip().split()
+    i,x = int(temp[0]), temp[1]
+    d[i].append(x)
     
-    for x in res:
-        for y in x: 
-            res2+=' '
-            res2+=str(y)
-    print(res2[1:])
-
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    arr = []
-
-    for _ in range(n):
-        arr.append(input().rstrip().split())
-
-    countSort(arr)
+kk = list(d.keys())
+kk.sort()
+ 
+for k in kk:
+    for x in d[k]:
+        print(x,end=' ')
+ 
