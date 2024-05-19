65de9539ca76de97742591c368f662510bfdbd2e
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-20 02:05:45 +0800

    Add files via upload

---
+++ Leetcode/gfg_Rod Cutting.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    # Function to find the maximum obtainable value by cutting the rod
+    def cutRod(self, price, n):
+
+        # Create a list to store the maximum obtainable value for each length of rod
+        val = [0]*(n+1)
+
+        # Iterate through each length of rod
+        for i in range(1,n+1):
+
+            # Initialize the maximum value to -1 for each length of rod
+            max_val = -1
+
+            # Iterate through each possible cut in the rod
+            for j in range(i):
+
+                # Calculate the maximum obtainable value by considering different cuts
+                max_val = max(max_val, price[j] + val[i - j - 1])
+
+            # Store the maximum obtainable value for the current length of rod
+            val[i] = max_val
+
+        # Return the maximum obtainable value for the given length of rod
+        return val[n]
