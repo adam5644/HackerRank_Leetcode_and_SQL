bf8ddd55593c48f7b9d8c987f8b76313e7570644
Adam Chen Longhui <longhuichen.2023@mqf.smu.edu.sg>
2024-05-24 22:00:12 +0800

    Add files via upload

---
+++ Leetcode/GFG_Count number of binary strings without consecutive 1’s.py
@@ -0,0 +1,16 @@
+# 
+def countStrings(n):
+ 
+    a=[0 for i in range(n)]
+    b=[0 for i in range(n)]
+    a[0] = b[0] = 1
+    for i in range(1,n):
+        a[i] = a[i-1] + b[i-1]
+        b[i] = a[i-1]
+     
+    return a[n-1] + b[n-1]
+ 
+# Driver program to test
+# above functions
+ 
+print(countStrings(3))
